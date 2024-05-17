56c4aa04312e14a1686aa450fbf84b7b34f6805a
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2024-05-17 17:09:22 +0100

    moved Placemark sites to a new component

--- src/routes/placemark/[id]/+page.svelte
+++ src/routes/placemark/[id]/+page.svelte
@@ -1,26 +1,9 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { page } from '$app/stores';
-    import { placemarkService } from '$lib/services/placemark-service';
-    import { get } from 'svelte/store';
-    import type { Placemark, Site } from '$lib/types/placemark-types';
-    import { currentSession, siteList } from '$lib/stores';
+
     import Card from '$lib/ui/Card.svelte';
     import SiteForm from './SiteForm.svelte';
-  
-    let placemark: Placemark | null = null;
-    let sites: Site[] = [];
-  
-    onMount(async () => {
-      const session = get(currentSession);
-      const { id } = get(page).params;
-      if (session && id) {
-        placemark = await placemarkService.getPlacemarkById(session, { _id: id, name: '', category: '' });
-        sites = await placemarkService.getPlacemarkSites(session, { _id: id, name: '', category: '' });
-        siteList.set(sites);
-    }
-    });
-  
+    import PlacemarkSiteList from '$lib/ui/PlacemarkSiteList.svelte';
+    
 
   </script>
   
@@ -28,31 +11,9 @@
 <Card title="Enter New Site Details here">
     <SiteForm />
 </Card>
+<Card title="All sites for this place of interest">
+    <PlacemarkSiteList />
+</Card>
 
-  <h1>Placemark: {placemark?.name}</h1>
-  <p>Category: {placemark?.category}</p>
-  
-  <h2>Sites</h2>
-  <table class="table is-fullwidth">
-    <thead>
-      <th>Title</th>
-      <th>Year</th>
-      <th>era</th>
-      <th>Lat</th>
-      <th>Lng</th>
-      <th>Description</th>
-    </thead>
-    <tbody>
-      {#each $siteList as site}
-        <tr>
-          <td>{site.title}</td>
-          <td>{site.year}</td>
-          <td>{site.era}</td>
-          <td>{site.latitude}</td>
-          <td>{site.longitude}</td>
-          <td>{site.description}</td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
+
   
