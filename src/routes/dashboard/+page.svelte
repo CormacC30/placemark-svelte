ece9be6c78276216718cc9c58bb38f67e8a130ca
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2024-05-18 17:23:09 +0100

    added site placemarks to map

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,59 +1,82 @@
 <script lang="ts">
-    // @ts-ignore
-    import Chart from "svelte-frappe-charts";
-    import Card from "$lib/ui/Card.svelte";
-    import { subTitle, currentSession } from "$lib/stores";
-    import PlacemarkForm from "./PlacemarkForm.svelte";
-    import PlacemarkList from "$lib/ui/PlacemarkList.svelte";
-    import type { Placemark, Session } from "$lib/types/placemark-types";
-    import type { ChartData } from "$lib/types/analytics-types";
-    import { placemarkStore } from "$lib/stores";
-    import { onMount } from "svelte";
-    import { placemarkService } from "$lib/services/placemark-service";
-    import { categorisePlacemarks, getPlacemarkChartData } from "$lib/services/analytics";
-    import { get } from "svelte/store";
-  
-    subTitle.set("Create a New Placemark");
-  
-    let session: Session;
-    let placemarks: Placemark[] = [];
-    let placemarkChartData: ChartData | null = null;
+  // @ts-ignore
+  import Chart from "svelte-frappe-charts";
+  import Card from "$lib/ui/Card.svelte";
+  import { subTitle, currentSession } from "$lib/stores";
+  import PlacemarkForm from "./PlacemarkForm.svelte";
+  import PlacemarkList from "$lib/ui/PlacemarkList.svelte";
+  import type { Placemark, Session } from "$lib/types/placemark-types";
+  import type { ChartData } from "$lib/types/analytics-types";
+  import { placemarkStore } from "$lib/stores";
+  import { onMount } from "svelte";
+  import { placemarkService } from "$lib/services/placemark-service";
+  import { categorisePlacemarks, getPlacemarkChartData } from "$lib/services/analytics";
+  import { get } from "svelte/store";
+  import LeafletMap from "$lib/ui/LeafletMap.svelte";
 
-    async function updateChart() {
+  subTitle.set("Create a New Placemark");
+
+  let session: Session;
+  let placemarks: Placemark[] = [];
+  let placemarkChartData: ChartData | null = null;
+  let map: LeafletMap;
+
+  async function updateChart() {
     const categories = categorisePlacemarks(placemarks);
     placemarkChartData = getPlacemarkChartData(categories);
   }
-  
-    onMount(async () => {
-      session = get(currentSession);
-      if (session) {
-        placemarks = await placemarkService.getPlacemarks(session);
-        placemarkStore.set(placemarks);
-        await updateChart();
-      }
-    });
 
-    placemarkStore.subscribe(async (newPlacemarks) => {
-      placemarks = newPlacemarks;
+  onMount(async () => {
+    const mapPlacemarks = await placemarkService.getPlacemarks(get(currentSession));
+    const sites = await placemarkService.getUserSites(get(currentSession));
+    let popup: string;
+
+    session = get(currentSession);
+    if (session) {
+      placemarks = await placemarkService.getPlacemarks(session);
+      placemarkStore.set(placemarks);
       await updateChart();
-    });
-  </script>
-  
-  <Card title="Please Add Place of Interest">
-    <PlacemarkForm />
-  </Card>
-  <div class="columns">
-    <div class="column">
-      <Card title="All Current Places of Interest:">
-        <PlacemarkList />
-      </Card>
-    </div>
-    <div class="column">
-      <Card title="Number of Placemarks per Category">
-        {#if placemarkChartData}
-          <Chart data={placemarkChartData} type="pie" />
-        {/if}
-      </Card>
-    </div>
+    }
+
+    for (const placemark of mapPlacemarks) {
+      sites.forEach((site) => {
+        popup = `${site.title}, Category: ${placemark.category}`;
+        map.addMarker(site.latitude, site.longitude, popup);
+      }); 
+    }
+    const lastSite = sites[sites.length -1];
+    if (lastSite) map.moveTo(lastSite.latitude, lastSite.longitude);
+  });
+
+  placemarkStore.subscribe(async (newPlacemarks) => {
+    placemarks = newPlacemarks;
+    await updateChart();
+  });
+</script>
+
+<div class="columns">
+  <div class="column">
+    <Card title="Please Add Place of Interest">
+      <PlacemarkForm />
+    </Card>
   </div>
-  
+  <div class="column">
+    <Card title="Historic Sites to Date">
+      <LeafletMap height={"30"} bind:this={map} />
+    </Card>
+  </div>
+</div>
+<div class="columns">
+  <div class="column">
+    <Card title="All Current Places of Interest:">
+      <PlacemarkList />
+    </Card>
+  </div>
+  <div class="column">
+    <Card title="Number of Placemarks per Category">
+      {#if placemarkChartData}
+        <Chart data={placemarkChartData} type="pie" />
+      {/if}
+    </Card>
+  </div>
+</div>
