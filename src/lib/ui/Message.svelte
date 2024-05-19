33707f427623cbd20a7f0673c763755006b80308
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2024-05-19 08:52:40 +0100

    signup flow, success message

--- src/lib/ui/Message.svelte
+++ src/lib/ui/Message.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
     export let message = "";
+    export let isSuccess = false;
+    let successColour = "is-success";
+    let failColour = "is-danger";
+
   </script>
   
-  <article class="message is-danger">
+  {#if isSuccess}
+  <article class="message {successColour}">
     <div class="message-body">
       {message}
     </div>
   </article>
+  {:else}
+  <article class="message {failColour}">
+    <div class="message-body">
+      {message}
+    </div>
+  </article>
+  {/if}
--- src/routes/signup/SignupForm.svelte
+++ src/routes/signup/SignupForm.svelte
@@ -3,17 +3,31 @@
     import UserCredentials from "$lib/ui/UserCredentials.svelte";
     import UserDetails from "$lib/ui/UserDetails.svelte";
     import Message from "$lib/ui/Message.svelte";
+    import { placemarkService } from "$lib/services/placemark-service";
+    import type { User } from "$lib/types/placemark-types";
   
     let firstName = "";
     let lastName = "";
     let email = "";
     let password = "";
     let message = "";
+    let isSuccess = false;
   
     async function signup() {
-      const success = false;
+      const user: User = {
+        firstName,
+        lastName,
+        email,
+        password
+      };
+
+      const success = await placemarkService.signup(user);
       if (success) {
-        goto("/dashboard");
+        message = "Signup Successful, Redirecting to Login Page...";
+        isSuccess = true;
+        setTimeout(() => {
+        goto("/login");
+      }, 2000);
       } else {
         message = "Error Trying to sign up";
       }
@@ -21,7 +35,7 @@
   </script>
   
   {#if message}
-    <Message {message} />
+    <Message {message} {isSuccess} />
   {/if}
   <form on:submit|preventDefault={signup}>
     <UserDetails bind:firstName bind:lastName />
