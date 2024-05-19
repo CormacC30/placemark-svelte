67ec7eeb99614cc2eb3ebaaf7d059dd3befe0296
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2024-05-19 16:34:38 +0100

    added year validation on site form

--- src/routes/placemark/[id]/SiteForm.svelte
+++ src/routes/placemark/[id]/SiteForm.svelte
@@ -5,25 +5,47 @@
     import { currentSession, siteList } from '$lib/stores';
     import Coordinates from '$lib/ui/Coordinates.svelte';
     import { sanitizeInput } from '$lib/services/utils';
+    import Message from '$lib/ui/Message.svelte';
 
     export let placemark: Placemark;
 
     let newSiteTitle = "";
-    let newSiteYear = 0;
+    let newSiteYear: number;
     let newSiteEra = ["BC", "AD"];
     let selectedEra = "";
     let latitude = 0;
     let longitude = 0;
     let newSiteDescription = "";
     let message = "";
+    let isSuccess: boolean | undefined = true;
+
+    function validateYear(year: number, era: string): boolean {
+    const currentYear = new Date().getFullYear();
+    if (era === "AD" && (year > currentYear || year < 0)) {
+      message = "Please enter a valid year";
+      isSuccess = false;
+      return false;
+    }
+    if (era === "BC" && year < 0) {
+      message = "Please enter a valid BC year";
+      isSuccess = false;
+      return false;
+    }
+    message = "";
+    isSuccess = true;
+    return true;
+  }
 
     async function addSite() {
       const session = get(currentSession);
 
       const sanitizedTitle = sanitizeInput(newSiteTitle);
-      
       const sanitizedEra = sanitizeInput(selectedEra); // probably unneccessary but I'll do it anyway
       const sanitizedDescription = sanitizeInput(newSiteDescription);
+
+      if(!validateYear(newSiteYear, selectedEra)){
+        return;
+      };
 
       if (!session || !placemark._id) return;
   
@@ -36,7 +58,7 @@
         era: sanitizedEra,
         placemarkid: placemark._id, 
       };
-  
+      
       const success = await placemarkService.addSite(session, placemark, site);
       if (success) {
         const sites = await placemarkService.getPlacemarkSites(session, placemark);
@@ -53,7 +75,7 @@
 <h2 class="title is-5">Add New Site</h2>
 <form on:submit|preventDefault={addSite}>
   <input bind:value={newSiteTitle} placeholder="Title" />
-  <input bind:value={newSiteYear} placeholder="Year" />
+  <input bind:value={newSiteYear} placeholder="Enter Year" min=0 />
   <label class="label" for="era">
       {#each newSiteEra as era}
       <input bind:group={selectedEra} class="radio" type="radio" value={era} /> {era}
@@ -63,4 +85,9 @@
   <textarea bind:value={newSiteDescription} placeholder="Description"></textarea>
   <button class="button is-primary">Add Site</button>
 </form>
-<p>{message}</p>
+{#if !isSuccess}
+<Message {message} {isSuccess} />
+{:else}
+  <p class="message">{message}</p>
+{/if}
+
