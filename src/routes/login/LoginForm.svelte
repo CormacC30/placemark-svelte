d74c9fa8761514445ee0cf4442bbc132dbc55d6b
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2024-05-19 08:59:16 +0100

    added success message to login

--- src/routes/login/LoginForm.svelte
+++ src/routes/login/LoginForm.svelte
@@ -1,33 +1,37 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
-    import { placemarkService } from "$lib/services/placemark-service";
-    import { currentSession } from "$lib/stores";
-    import Message from "$lib/ui/Message.svelte";
-    import UserCredentials from "$lib/ui/UserCredentials.svelte";
-  
-    let email = "";
-    let password = "";
-    let message = "";
-  
-    async function login() {
-        console.log(`attempting to log in email: ${email} with password: ${password}`);
-        let session = await placemarkService.login(email, password);
-        if (session) {
-            currentSession.set(session);
-            localStorage.placemark = JSON.stringify(session);
-            goto("/dashboard");
-        } else {
-            email = "",
-            password = "",
-            message = "Invalid Credentials";
-        }
+  import { goto } from "$app/navigation";
+  import { placemarkService } from "$lib/services/placemark-service";
+  import { currentSession } from "$lib/stores";
+  import Message from "$lib/ui/Message.svelte";
+  import UserCredentials from "$lib/ui/UserCredentials.svelte";
+
+  let email = "";
+  let password = "";
+  let message = "";
+  let isSuccess = false;
+
+  async function login() {
+    console.log(`attempting to log in email: ${email} with password: ${password}`);
+    let session = await placemarkService.login(email, password);
+    if (session) {
+      message = "Login Successful";
+      isSuccess = true;
+
+      setTimeout(() => {
+        currentSession.set(session);
+      localStorage.placemark = JSON.stringify(session);
+        goto("/dashboard");
+      }, 1000);
+    } else {
+      (email = ""), (password = ""), (message = "Invalid Credentials");
     }
-  </script>
-  
-  {#if message}
-    <Message {message} />
-  {/if}
-  <form on:submit|preventDefault={login}>
-    <UserCredentials bind:email bind:password />
-    <button class="button is-success is-fullwidth">Log In</button>
-  </form>
+  }
+</script>
+
+{#if message}
+  <Message {message} {isSuccess} />
+{/if}
+<form on:submit|preventDefault={login}>
+  <UserCredentials bind:email bind:password />
+  <button class="button is-success is-fullwidth">Log In</button>
+</form>
