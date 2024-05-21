9615ec07d9b1c2955be2ca4531a09a92c4155166
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2024-05-21 01:25:26 +0100

    more ui fixes

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -6,10 +6,10 @@
   import PlacemarkForm from "./PlacemarkForm.svelte";
   import PlacemarkList from "$lib/ui/PlacemarkList.svelte";
   import type { Placemark, Session, Site } from "$lib/types/placemark-types";
-  import type { ChartData } from "$lib/types/analytics-types";
+  import type { ChartData, NewChartData } from "$lib/types/analytics-types";
   import { onMount } from "svelte";
   import { placemarkService } from "$lib/services/placemark-service";
-  import { categorisePlacemarks, getPlacemarkChartData, getSitesWithCategories } from "$lib/services/analytics";
+  import { categorisePlacemarks, getPlacemarkChartData, getSitesWithCategories, getAllByCategory } from "$lib/services/analytics";
   import { get } from "svelte/store";
   import LeafletMap from "$lib/ui/LeafletMap.svelte";
 
@@ -19,12 +19,22 @@
   let placemarks: Placemark[] = [];
   let sites: Site[] = [];
   let placemarkChartData: ChartData | null = null;
+  let combinedChartData: NewChartData | null = null;
   let map: LeafletMap;
   let isEmpty = true;
 
   async function updateChart() {
     const categories = categorisePlacemarks(placemarks);
+    const { labels, placemarkCounts, siteCounts } = getAllByCategory(placemarks, sites);
     placemarkChartData = getPlacemarkChartData(categories);
+
+    combinedChartData = {
+      labels,
+      datasets: [
+        { name: "Placemarks", type: "bar", values: placemarkCounts },
+        { name: "Sites", type: "bar", values: siteCounts }
+      ]
+    };
   }
 
   onMount(async () => {
@@ -45,6 +55,7 @@
       const popup = `${siteCat.site.title}, Category: ${siteCat.category}`;
       map.addMarker(siteCat.site.latitude, siteCat.site.longitude, popup, siteCat.category )
     });
+
   });
 
   placemarkStore.subscribe(async (newPlacemarks) => {
@@ -68,28 +79,32 @@
 
     const lastSite = sites[sites.length - 1];
     if (lastSite) map.moveTo(lastSite.latitude, lastSite.longitude);
+
+    updateChart();
   });
 </script>
 
 <div class="columns">
-  <div class="column">
+  <div class="column is-three-fifths">
     <Card title="Please Add Place of Interest">
       <PlacemarkForm />
+      <PlacemarkList />
     </Card>
   </div>
-  <div class="column">
-    <Card title="Historic Sites to Date">
-      <LeafletMap height={"30"} bind:this={map} />
+  <div class="column is-auto">
+    <Card title="Placemarks and Sites per Category">
+      <Chart data={combinedChartData} type="bar" />
     </Card>
   </div>
 </div>
 <div class="columns">
+
   <div class="column">
-    <Card title="All Current Places of Interest:">
-      <PlacemarkList />
+    <Card title="Historic Sites Locations">
+      <LeafletMap height={"30"} bind:this={map} />
     </Card>
   </div>
-  <div class="column">
+  <div class="column is-one-quarter">
     <Card title="Number of Placemarks per Category">
       {#if isEmpty}
         <h3 class="message">
@@ -97,9 +112,10 @@
         </h3>
       {:else if !isEmpty}
         {#if placemarkChartData}
-          <Chart data={placemarkChartData} type="pie" />
+          <Chart data={placemarkChartData} type="donut" />
         {/if}
       {/if}
     </Card>
   </div>
 </div>
+
--- src/routes/dashboard/PlacemarkForm.svelte
+++ src/routes/dashboard/PlacemarkForm.svelte
@@ -4,9 +4,9 @@
   import { get } from 'svelte/store';
   import { sanitizeInput } from '$lib/services/utils';
 
-  let inputName = '';
-  let inputCategory = '';
-  let message = '';
+  let inputName = "";
+  let inputCategory = "";
+  let message = "";
   let isSuccess = true;
   const categories = ['Bronze Age', 'Iron Age', 'Medieval', 'Industrial Revolution', 'Famine', 'Early Christian', 'Neolithic Period', 'Mesolithic Period', 'War of Independance', 'Civil War' ];
 
@@ -34,16 +34,31 @@
   }
 </script>
 
-<form on:submit|preventDefault={addPlacemark}>
-  <input bind:value={inputName} placeholder="Name of the Placemark" />
-  <select bind:value={inputCategory}>
-    {#each categories as categoryOption}
-      <option value={categoryOption}>{categoryOption}</option>
-    {/each}
-  </select>
-  <button class="button is-success">Add Placemark</button>
+<div class="box field is-horizontal">
+  <div class="field-label is-normal">
+    <label for="Placemark Name" class="label">Name</label>
+  </div>
+  <div class="field-body">
+    <div class="field">
+      <p class="control is-expanded">
+        <input id="Name" class="input" type="string" bind:value={inputName} Placeholder="Name of the Placemark">
+      </p>
+    </div>
+    <div class="field-label is-normal">
+      <label for="Category" class="label">Category</label>
+    </div>
+    <div class="field center">
+      <select bind:value={inputCategory}>
+        {#each categories as categoryOption}
+          <option value={categoryOption}>{categoryOption}</option>
+        {/each}
+      </select>
+    </div>
+  </div>
+  <button on:submit|preventDefault={addPlacemark} class="button is-success">Add Placemark</button>
+</div>
 
-</form>
+
 {#if !isSuccess}
 <div class="box mt-5">
   <div class="content has-text-centered">
